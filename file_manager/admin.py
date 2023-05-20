7c5efd598cbfe049c7d0651fd5f1053120f68928
dy1owens <dylowens@gmail.com>
2023-05-20 15:43:32 -0700

    Update admin.py

--- file_manager/admin.py
+++ file_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.gis.admin import GISModelAdmin
+# from django.contrib.gis.admin import GISModelAdmin
 from .models import UploadedFile
 # Register your models here.
 admin.site.register(UploadedFile)
