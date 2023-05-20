6ae83f73130c6094889f81fdff208fb3baab3e8a
dylowens <127639598+dylowens@users.noreply.github.com>
2023-05-20 14:48:27 -0700

    Update settings.py

--- owens_maps_django/settings.py
+++ owens_maps_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-03-b00lf=u1jm@5!@gw!ht8@&4%&ko+$dx_-dw-%s-6@pxka+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.33.36.74']
 
 
 # Application definition
