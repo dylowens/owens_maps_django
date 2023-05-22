35972d32f20a7fd199b629c12c744fc28b503596
dy1owens <dylowens@gmail.com>
2023-05-21 20:12:32 -0700

    update settings.py

--- owens_maps_django/settings.py
+++ owens_maps_django/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 
 # This is where Django will look for your static files
 STATICFILES_DIRS = [
