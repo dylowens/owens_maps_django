4c6387bc6638ccf21d9e6bf913537fa4d5a8187c
dy1owens <dylowens@gmail.com>
2023-05-21 20:46:10 -0700

    urls.py update

--- owens_maps_django/urls.py
+++ owens_maps_django/urls.py
@@ -21,10 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('imagetotext', include('imagetotext.urls')),
+    path('imagetotext/', include('imagetotext.urls')),
     path('', include('homepage.urls')),
     # path('esri_app/', include('esri_app.urls')),
-    path('file_manager', include('file_manager.urls')),
+    path('file_manager/', include('file_manager.urls')),
     # path('text_to_speech/', include('texttospeech.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
