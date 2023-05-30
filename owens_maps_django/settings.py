fc167480a1bcf34939b60d4fa5452796e92f8adc
dy1owens <dylowens@gmail.com>
2023-05-29 18:05:46 -0700

    Update settings.py

--- owens_maps_django/settings.py
+++ owens_maps_django/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'owens_maps_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'owensmaps',
+        'USER': 'admin',
+        'PASSWORD': 'adminpassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
